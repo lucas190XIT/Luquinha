1198cd2bf04d639242cac6fe2dc7259927e3e518
lucas190XIT <joselucas96988@gmail.com>
2024-12-02 11:40:25 -0300

    Add files via upload

---
+++ calculadora3.0.py
@@ -0,0 +1,8 @@
+teste = input("selecione entre (+, -, %)")
+num1 = float(input ("numero1"))
+num2 = float(input ("numero2"))
+testing = {'+': num1 + num2}
+print("resultado:", testing.get(teste, )) 
+if num1 + num2 == 4: {
+    print ("Yea")}
+    
